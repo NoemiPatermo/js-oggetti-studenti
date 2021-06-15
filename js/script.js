1b58844d76c78d10e908256fad7ee4531f5ffef1
noemi86-cpu <noemipatermo@gmail.com>
2021-06-15 18:51:53 +0200

    js-studenti

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell’oggetto.
 
 var studentList = {
-    student : [{name: "Noemi", surname: "Patermo", età: "35"}]
+    student : [{name: "Noemi", surname: "Patermo", età: "35"}] //FORSE NON DOVEVO CREARE ARRAY
 
 }
 for (var x = 0; x < studentList.student.length; x++) {
@@ -12,8 +12,9 @@ for (var x = 0; x < studentList.student.length; x++) {
     for (var k in studentList.student[x]) {
         
         console.log(k,studentList.student[x][k])
-    }
     
+   document.getElementById("output").innerHTML +=  studentList.student[x] [k] + " <br>  "; 
+    }
 }
 //PARTE 2
 //Creare un array di oggetti di studenti. 
@@ -22,35 +23,47 @@ for (var x = 0; x < studentList.student.length; x++) {
 
 //crea array di oggetti di studenti
 
-var studentList = [
+var arrayList = [
     {name: "Noemi", surname: "Patermo", età: "35"},
     {name: "Pietro", surname: "Patermo", età: "33" },
     {name: "Eliana", surname: "Patermo", età: "30"}
-    ];
-for (var x = 0; x < studentList.length; x++){
-    console.log(studentList[x])
-}
+    ]
 
+    for (var x = 0; x < arrayList.length; x++){
+        console.log(arrayList[x].name) //richiamo col . la proprietà che mi interessa far stamapre 
+        console.log(arrayList[x].surname)
+
+    //ALTRO MODO è IL FOR IN 
+        for (var k in arrayList[x]) {
+        console.log(k, arrayList[x][k])
+    
+     document.getElementById("output2").innerHTML += arrayList[x] [k] + "   " ;
+    }
+ }
 
 //PARTE 3
 //Dare la possibilità all’utente, attraverso 3 prompt() - uno per ciascuna proprietà - di 
 //aggiungere UN SOLO oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-/* OPPURE
 
-var studentList = {
+
+var classList = {
     student : []
 }
-while (studentList.student.length < 1){
     var nameStudent = prompt("Inserisci il nome ")
     var surnameStudent = prompt(" Inserisci il cognome ")
     var ageStudent = prompt(" Inserisci l'età ")
-    studentList.student.push({
+
+    classList.student.push({
         name: nameStudent,
         surname: surnameStudent, 
         age : ageStudent 
     });
+
+for(var x = 0; x < classList.student.length; x++){
+    for (var k in classList.student[x]) {
+        document.getElementById("output3").innerHTML += classList.student[x][k];
+    }
 }
-console.log(studentList);
-*/
+
 
