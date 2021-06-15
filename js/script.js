8d74d94355030fa7212338c7d9ad0b0d262c7143
noemi86-cpu <noemipatermo@gmail.com>
2021-06-15 14:54:09 +0200

    js-oggetti

---
+++ js/script.js
@@ -0,0 +1,3 @@
+//PARTE 1
+//Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell’oggetto.
