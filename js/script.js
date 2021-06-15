dcb87be24c79dcbc4d4aa045a0653cb374dff6ba
noemi86-cpu <noemipatermo@gmail.com>
2021-06-15 17:16:22 +0200

    js-studenti

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 //PARTE 1
 //Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell’oggetto.
+
 var studentList = {
-    student : [{name: "Noemi", surname: "Patermo", età: "35"},
-                {name: "Pietro", surname: "Patermo", età: "33" },
-                {name: "Gabriele", surname: "Sanna", età: "21"}]
+    student : [{name: "Noemi", surname: "Patermo", età: "35"}]
 
 }
 for (var x = 0; x < studentList.student.length; x++) {
@@ -16,6 +15,26 @@ for (var x = 0; x < studentList.student.length; x++) {
     }
     
 }
+//PARTE 2
+//Creare un array di oggetti di studenti. 
+//Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.  
+//Ricordatevi, è un array, quindi? for.
+
+//crea array di oggetti di studenti
+
+var studentList = [
+    {name: "Noemi", surname: "Patermo", età: "35"},
+    {name: "Pietro", surname: "Patermo", età: "33" },
+    {name: "Eliana", surname: "Patermo", età: "30"}
+    ];
+for (var x = 0; x < studentList.length; x++){
+    console.log(studentList[x])
+}
+
+
+//PARTE 3
+//Dare la possibilità all’utente, attraverso 3 prompt() - uno per ciascuna proprietà - di 
+//aggiungere UN SOLO oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 /* OPPURE
 
@@ -34,8 +53,4 @@ while (studentList.student.length < 1){
 }
 console.log(studentList);
 */
-//PARTE 2
-//Creare un array di oggetti di studenti. 
-//Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.  
-//Ricordatevi, è un array, quindi? for.
-    
+
